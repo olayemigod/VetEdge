2f818b3b6359739f246752e4da7a8b691f32d2bd
olayemigod <olayemigod@gmail.com>
2026-07-20 22:04:59 +0100

    fix(vetedge): scope registration invoice currency to patient company

--- vetedge/veterinary/doctype/veterinary_patient/veterinary_patient.py
+++ vetedge/veterinary/doctype/veterinary_patient/veterinary_patient.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from frappe.model.document import Document
 
+from vetedge.services.appointment_quick_create_safety import registration_invoice_context
 from vetedge.services.copy_control import reset_vetedge_copy_state
 from vetedge.services.patient import validate_patient
 from vetedge.services.registration_billing import handle_patient_registration_insert
@@ -13,4 +14,5 @@ class VeterinaryPatient(Document):
 		validate_patient(self)
 
 	def after_insert(self) -> None:
-		handle_patient_registration_insert(self)
+		with registration_invoice_context(self):
+			handle_patient_registration_insert(self)
