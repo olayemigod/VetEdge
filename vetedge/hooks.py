285e567914ba61a7f484c114946b63637d5a37a2
olayemigod <olayemigod@gmail.com>
2026-07-20 01:07:16 +0100

    chore(vetedge): refresh shared UI bridge asset version

--- vetedge/hooks.py
+++ vetedge/hooks.py
@@ -33,7 +33,7 @@ app_include_css = [
 app_include_js = [
 	"/assets/vetedge/js/edgesuite_product_menu.js?v=20260718-3",
 	"/assets/vetedge/js/vetedge_professional_ui.js?v=20260719-1",
-	"/assets/vetedge/js/vetedge_ui_bridge.js?v=20260720-1",
+	"/assets/vetedge/js/vetedge_ui_bridge.js?v=20260720-2",
 	"/assets/vetedge/js/edgesuite_date_ranges.js",
 	"/assets/vetedge/js/dashboard_shell.js",
 	"/assets/vetedge/js/invoice_summary_dialog.js",
