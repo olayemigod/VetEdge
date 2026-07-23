ec4129aa14cf2d5bcad8a5b6d57e1dd6444ffa63
olayemigod <olayemigod@gmail.com>
2026-07-23 12:31:09 +0100

    fix(vetedge): clarify pricing master status filters

--- vetedge/public/js/vetedge_pricing_master_workspace.bundle.js
+++ vetedge/public/js/vetedge_pricing_master_workspace.bundle.js
@@ -1,6 +1,55 @@
 import VetEdgePricingMasterWorkspace from './vetedge_pricing_master_workspace/VetEdgePricingMasterWorkspace.vue';
 import { applyWorkspaceSafety } from './vetedge_workspace_safety';
 
+function installStatusFilterSemantics(component) {
+	const methods = component?.methods;
+	if (!methods || component.__vetedgePricingStatusFiltersInstalled) return component;
+
+	const originalLoadCurrentRoute = methods.loadCurrentRoute;
+	methods.loadCurrentRoute = async function (...args) {
+		const result = await originalLoadCurrentRoute.apply(this, args);
+		const filters = (this.definition?.filters || []).map((field) => {
+			if (field.fieldname !== 'disabled') return field;
+			return {
+				...field,
+				fieldname: 'is_active',
+				label: __('Status'),
+				source_fieldname: 'disabled',
+			};
+		});
+		this.definition = { ...this.definition, filters };
+		return result;
+	};
+
+	const originalLoadList = methods.loadList;
+	methods.loadList = async function (...args) {
+		const hasDisabledAlias = (this.definition?.filters || []).some(
+			(field) => field.source_fieldname === 'disabled'
+		);
+		if (!hasDisabledAlias || !Object.prototype.hasOwnProperty.call(this.filters || {}, 'is_active')) {
+			return originalLoadList.apply(this, args);
+		}
+
+		const originalFilters = this.filters;
+		const translatedFilters = { ...originalFilters };
+		const activeValue = translatedFilters.is_active;
+		delete translatedFilters.is_active;
+		if (activeValue !== '') {
+			translatedFilters.disabled = String(activeValue) === '1' ? '0' : '1';
+		}
+		this.filters = translatedFilters;
+		try {
+			return await originalLoadList.apply(this, args);
+		} finally {
+			this.filters = originalFilters;
+		}
+	};
+
+	component.__vetedgePricingStatusFiltersInstalled = true;
+	return component;
+}
+
+installStatusFilterSemantics(VetEdgePricingMasterWorkspace);
 applyWorkspaceSafety(VetEdgePricingMasterWorkspace);
 
 export function mountVetEdgePricingMasterWorkspace(target) {
