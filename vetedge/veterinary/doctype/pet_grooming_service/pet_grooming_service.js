23b59c7e7a9181fca3852358b4954a377577c055
olayemigod <olayemigod@gmail.com>
2026-07-23 12:21:54 +0100

    feat(vetedge): route grooming service forms to EdgeSuite

---
+++ vetedge/veterinary/doctype/pet_grooming_service/pet_grooming_service.js
@@ -0,0 +1,8 @@
+frappe.ui.form.on('Pet Grooming Service', {
+	refresh(frm) {
+		const query = frm.is_new()
+			? 'resource=grooming-services&new=1'
+			: `resource=grooming-services&name=${encodeURIComponent(frm.doc.name)}`;
+		window.location.replace(`/app/vetedge-pricing-master-workspace?${query}`);
+	}
+});
