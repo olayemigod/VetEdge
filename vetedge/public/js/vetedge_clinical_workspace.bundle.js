e9ab6e159d7a6f3a25c819212c91b38e7c76b865
olayemigod <olayemigod@gmail.com>
2026-07-23 18:16:29 +0100

    fix(vetedge): harden clinical workspace treatment and vitals behaviour

--- vetedge/public/js/vetedge_clinical_workspace.bundle.js
+++ vetedge/public/js/vetedge_clinical_workspace.bundle.js
@@ -1,6 +1,28 @@
 import VetEdgeClinicalWorkspace from './vetedge_clinical_workspace/VetEdgeClinicalWorkspace.vue';
 import { applyWorkspaceSafety } from './vetedge_workspace_safety';
 
+const originalTreatmentRowLocked = VetEdgeClinicalWorkspace.methods?.treatmentRowLocked;
+VetEdgeClinicalWorkspace.methods.treatmentRowLocked = function treatmentRowLockedWithSourceProtection(row) {
+	const sourceGenerated = Boolean(
+		row?.source_document
+		|| row?.source_detail_name
+		|| ['Consultation', 'Lab Order', 'Vaccination'].includes(row?.source_type)
+	);
+	return sourceGenerated || originalTreatmentRowLocked?.call(this, row) === true;
+};
+
+const originalSaveVitals = VetEdgeClinicalWorkspace.methods?.saveVitals;
+if (typeof originalSaveVitals === 'function') {
+	VetEdgeClinicalWorkspace.methods.saveVitals = async function saveVitalsWithReliableClose() {
+		const previousVitals = this.detail?.latest_vitals?.name || '';
+		await originalSaveVitals.call(this);
+		const currentVitals = this.detail?.latest_vitals?.name || '';
+		if (!this.busy && currentVitals && currentVitals !== previousVitals) {
+			this.vitalsDialog = { open: false, values: {} };
+		}
+	};
+}
+
 applyWorkspaceSafety(VetEdgeClinicalWorkspace, { guardNavigation: true });
 
 export function mountVetEdgeClinicalWorkspace(target) {
