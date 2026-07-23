3e2312b6df6b45784963b53bb0f379b9dd2c19c5
olayemigod <olayemigod@gmail.com>
2026-07-23 12:20:50 +0100

    feat(vetedge): route treatment type forms to EdgeSuite

---
+++ vetedge/veterinary/doctype/veterinary_treatment_type/veterinary_treatment_type.js
@@ -0,0 +1,8 @@
+frappe.ui.form.on('Veterinary Treatment Type', {
+	refresh(frm) {
+		const query = frm.is_new()
+			? 'resource=treatment-types&new=1'
+			: `resource=treatment-types&name=${encodeURIComponent(frm.doc.name)}`;
+		window.location.replace(`/app/vetedge-pricing-master-workspace?${query}`);
+	}
+});
