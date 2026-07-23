78ff27a717070c72dc67bc5bdc97073552fe1f9d
olayemigod <olayemigod@gmail.com>
2026-07-23 13:24:38 +0100

    feat(vetedge): route guest request list to action center

--- vetedge/veterinary/doctype/veterinary_guest_booking_request/veterinary_guest_booking_request_list.js
+++ vetedge/veterinary/doctype/veterinary_guest_booking_request/veterinary_guest_booking_request_list.js
@@ -1,1 +1,5 @@
-frappe.listview_settings["Veterinary Guest Booking Request"] = {};
+frappe.listview_settings['Veterinary Guest Booking Request'] = {
+	onload() {
+		window.location.replace('/app/vetedge-front-desk-action-center?tab=guest');
+	}
+};
