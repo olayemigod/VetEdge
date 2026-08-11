f9bb045e63b9746739a99e8b27f4b9b32a69153c
olayemigod <olayemigod@gmail.com>
2026-08-11 10:49:46 +0100

    perf(vetedge): require searchable stock expiry link field

--- vetedge/veterinary/page/stock_expiry_monitor/stock_expiry_monitor.js
+++ vetedge/veterinary/page/stock_expiry_monitor/stock_expiry_monitor.js
@@ -57,6 +57,7 @@ frappe.pages['stock-expiry-monitor'].on_page_show = function(wrapper) {
 		'EdgePageLayout',
 		'EdgePageHeader',
 		'EdgeFilterBar',
+		'EdgeLinkField',
 		'EdgeStatCard',
 		'EdgeStatusBadge',
 		'EdgeLoadingState',
