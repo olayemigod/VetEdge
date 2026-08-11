ebfea1971834b5ca60b07e13d72644eccd205fa8
olayemigod <olayemigod@gmail.com>
2026-08-12 00:40:01 +0100

    fix(vetedge): route lab test list through desk

--- vetedge/veterinary/doctype/veterinary_lab_test/veterinary_lab_test_list.js
+++ vetedge/veterinary/doctype/veterinary_lab_test/veterinary_lab_test_list.js
@@ -1,5 +1,5 @@
 frappe.listview_settings['Veterinary Lab Test'] = {
 	onload() {
-		window.location.replace('/app/vetedge-pricing-master-workspace?resource=lab-tests');
+		window.location.replace('/desk/vetedge-pricing-master-workspace?resource=lab-tests');
 	}
 };
