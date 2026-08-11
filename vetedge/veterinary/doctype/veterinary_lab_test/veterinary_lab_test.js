3069b3b3757e216563c94714406e4f04e76d1f08
olayemigod <olayemigod@gmail.com>
2026-08-12 00:39:48 +0100

    fix(vetedge): route lab test forms through desk

--- vetedge/veterinary/doctype/veterinary_lab_test/veterinary_lab_test.js
+++ vetedge/veterinary/doctype/veterinary_lab_test/veterinary_lab_test.js
@@ -3,6 +3,6 @@ frappe.ui.form.on('Veterinary Lab Test', {
 		const query = frm.is_new()
 			? 'resource=lab-tests&new=1'
 			: `resource=lab-tests&name=${encodeURIComponent(frm.doc.name)}`;
-		window.location.replace(`/app/vetedge-pricing-master-workspace?${query}`);
+		window.location.replace(`/desk/vetedge-pricing-master-workspace?${query}`);
 	}
 });
