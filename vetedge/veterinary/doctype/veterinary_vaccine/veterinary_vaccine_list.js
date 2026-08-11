45c6162711d8fa3b37a2ab87b2fe393a94de76ad
olayemigod <olayemigod@gmail.com>
2026-08-12 00:38:36 +0100

    fix(vetedge): route vaccine list through desk

--- vetedge/veterinary/doctype/veterinary_vaccine/veterinary_vaccine_list.js
+++ vetedge/veterinary/doctype/veterinary_vaccine/veterinary_vaccine_list.js
@@ -1,5 +1,5 @@
 frappe.listview_settings['Veterinary Vaccine'] = {
 	onload() {
-		window.location.replace('/app/vetedge-pricing-master-workspace?resource=vaccines');
+		window.location.replace('/desk/vetedge-pricing-master-workspace?resource=vaccines');
 	}
 };
