711496a929a54c4b27b2125ccf81cbcfafff3031
olayemigod <olayemigod@gmail.com>
2026-08-12 00:39:21 +0100

    fix(vetedge): route treatment type forms through desk

--- vetedge/veterinary/doctype/veterinary_treatment_type/veterinary_treatment_type.js
+++ vetedge/veterinary/doctype/veterinary_treatment_type/veterinary_treatment_type.js
@@ -3,6 +3,6 @@ frappe.ui.form.on('Veterinary Treatment Type', {
 		const query = frm.is_new()
 			? 'resource=treatment-types&new=1'
 			: `resource=treatment-types&name=${encodeURIComponent(frm.doc.name)}`;
-		window.location.replace(`/app/vetedge-pricing-master-workspace?${query}`);
+		window.location.replace(`/desk/vetedge-pricing-master-workspace?${query}`);
 	}
 });
