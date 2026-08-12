aa60107a9ec1e9025355ccd4a0a545c07182d9ec
olayemigod <olayemigod@gmail.com>
2026-08-13 00:27:31 +0100

    feat(vetedge): load dashboard alignment on practitioner dashboard

--- vetedge/veterinary/page/vetedge_practitioner_performance_dashboard/vetedge_practitioner_performance_dashboard.js
+++ vetedge/veterinary/page/vetedge_practitioner_performance_dashboard/vetedge_practitioner_performance_dashboard.js
@@ -1,8 +1,11 @@
 frappe.pages["vetedge-practitioner-performance-dashboard"].on_page_load = function (wrapper) {
 	frappe.require("vetedge_dashboard_host.bundle.js", function () {
-		window.mountVetEdgeDashboardHost(wrapper, {
-			key: "practitioner_performance",
-			title: __("Practitioner Performance Dashboard"),
+		frappe.require("vetedge_dashboard_alignment.bundle.js", function () {
+			window.VetEdgeDashboardAlignment?.install?.();
+			window.mountVetEdgeDashboardHost(wrapper, {
+				key: "practitioner_performance",
+				title: __("Practitioner Performance Dashboard"),
+			});
 		});
 	});
 };
