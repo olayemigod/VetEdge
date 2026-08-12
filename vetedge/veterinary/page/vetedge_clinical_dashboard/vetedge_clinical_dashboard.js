02261f67f7f1b4bb0882c9797397d3e48aa27fd9
olayemigod <olayemigod@gmail.com>
2026-08-12 20:42:49 +0100

    feat(vetedge): mount clinical dashboard in EdgeSuite shell

--- vetedge/veterinary/page/vetedge_clinical_dashboard/vetedge_clinical_dashboard.js
+++ vetedge/veterinary/page/vetedge_clinical_dashboard/vetedge_clinical_dashboard.js
@@ -1,6 +1,8 @@
 frappe.pages["vetedge-clinical-dashboard"].on_page_load = function (wrapper) {
-	const page = frappe.ui.make_app_page({ parent: wrapper, title: __("Clinical Dashboard"), single_column: true });
-	frappe.require("/assets/vetedge/js/dashboard_shell.js", function () {
-		window.vetedgeDashboardShell.mount(page, { key: "clinical", title: __("Clinical Dashboard") });
+	frappe.require("vetedge_dashboard_host.bundle.js", function () {
+		window.mountVetEdgeDashboardHost(wrapper, {
+			key: "clinical",
+			title: __("Clinical Dashboard"),
+		});
 	});
 };
