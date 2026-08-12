870cf7c63ef6c7472ce5effcf79f4e676875d6f7
olayemigod <olayemigod@gmail.com>
2026-08-13 00:26:56 +0100

    feat(vetedge): load dashboard alignment on vaccination dashboard

--- vetedge/veterinary/page/vetedge_vaccination_dashboard/vetedge_vaccination_dashboard.js
+++ vetedge/veterinary/page/vetedge_vaccination_dashboard/vetedge_vaccination_dashboard.js
@@ -1,8 +1,11 @@
 frappe.pages["vetedge-vaccination-dashboard"].on_page_load = function (wrapper) {
 	frappe.require("vetedge_dashboard_host.bundle.js", function () {
-		window.mountVetEdgeDashboardHost(wrapper, {
-			key: "vaccination",
-			title: __("Vaccination Dashboard"),
+		frappe.require("vetedge_dashboard_alignment.bundle.js", function () {
+			window.VetEdgeDashboardAlignment?.install?.();
+			window.mountVetEdgeDashboardHost(wrapper, {
+				key: "vaccination",
+				title: __("Vaccination Dashboard"),
+			});
 		});
 	});
 };
