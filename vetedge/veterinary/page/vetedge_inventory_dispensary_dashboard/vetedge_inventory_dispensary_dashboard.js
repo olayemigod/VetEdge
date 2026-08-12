637d2b694b357848bdf363d8b183ac22ff28e961
olayemigod <olayemigod@gmail.com>
2026-08-12 20:43:11 +0100

    feat(vetedge): mount inventory dashboard in EdgeSuite shell

--- vetedge/veterinary/page/vetedge_inventory_dispensary_dashboard/vetedge_inventory_dispensary_dashboard.js
+++ vetedge/veterinary/page/vetedge_inventory_dispensary_dashboard/vetedge_inventory_dispensary_dashboard.js
@@ -1,6 +1,8 @@
 frappe.pages["vetedge-inventory-dispensary-dashboard"].on_page_load = function (wrapper) {
-	const page = frappe.ui.make_app_page({ parent: wrapper, title: __("Inventory / Dispensary Dashboard"), single_column: true });
-	frappe.require("/assets/vetedge/js/dashboard_shell.js", function () {
-		window.vetedgeDashboardShell.mount(page, { key: "inventory_dispensary", title: __("Inventory / Dispensary Dashboard") });
+	frappe.require("vetedge_dashboard_host.bundle.js", function () {
+		window.mountVetEdgeDashboardHost(wrapper, {
+			key: "inventory_dispensary",
+			title: __("Inventory / Dispensary Dashboard"),
+		});
 	});
 };
