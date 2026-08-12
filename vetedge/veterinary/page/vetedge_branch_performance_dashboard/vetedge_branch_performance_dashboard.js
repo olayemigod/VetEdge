d009eb58f648f07a2b6d086effc20977a4953434
olayemigod <olayemigod@gmail.com>
2026-08-13 00:27:43 +0100

    feat(vetedge): load dashboard alignment on branch dashboard

--- vetedge/veterinary/page/vetedge_branch_performance_dashboard/vetedge_branch_performance_dashboard.js
+++ vetedge/veterinary/page/vetedge_branch_performance_dashboard/vetedge_branch_performance_dashboard.js
@@ -1,8 +1,11 @@
 frappe.pages["vetedge-branch-performance-dashboard"].on_page_load = function (wrapper) {
 	frappe.require("vetedge_dashboard_host.bundle.js", function () {
-		window.mountVetEdgeDashboardHost(wrapper, {
-			key: "branch_performance",
-			title: __("Branch Performance Dashboard"),
+		frappe.require("vetedge_dashboard_alignment.bundle.js", function () {
+			window.VetEdgeDashboardAlignment?.install?.();
+			window.mountVetEdgeDashboardHost(wrapper, {
+				key: "branch_performance",
+				title: __("Branch Performance Dashboard"),
+			});
 		});
 	});
 };
