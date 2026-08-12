dee967871412d8f555c51574786cc677005510ca
olayemigod <olayemigod@gmail.com>
2026-08-12 20:43:29 +0100

    feat(vetedge): mount vaccination dashboard in EdgeSuite shell

--- vetedge/veterinary/page/vetedge_vaccination_dashboard/vetedge_vaccination_dashboard.js
+++ vetedge/veterinary/page/vetedge_vaccination_dashboard/vetedge_vaccination_dashboard.js
@@ -1,6 +1,8 @@
 frappe.pages["vetedge-vaccination-dashboard"].on_page_load = function (wrapper) {
-	const page = frappe.ui.make_app_page({ parent: wrapper, title: __("Vaccination Dashboard"), single_column: true });
-	frappe.require("/assets/vetedge/js/dashboard_shell.js", function () {
-		window.vetedgeDashboardShell.mount(page, { key: "vaccination", title: __("Vaccination Dashboard") });
+	frappe.require("vetedge_dashboard_host.bundle.js", function () {
+		window.mountVetEdgeDashboardHost(wrapper, {
+			key: "vaccination",
+			title: __("Vaccination Dashboard"),
+		});
 	});
 };
