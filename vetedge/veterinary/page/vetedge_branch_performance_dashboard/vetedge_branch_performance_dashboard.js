469a542f9ad446e41bbee7e948ebc6111f2f94b3
olayemigod <olayemigod@gmail.com>
2026-08-12 20:44:17 +0100

    feat(vetedge): mount branch dashboard in EdgeSuite shell

--- vetedge/veterinary/page/vetedge_branch_performance_dashboard/vetedge_branch_performance_dashboard.js
+++ vetedge/veterinary/page/vetedge_branch_performance_dashboard/vetedge_branch_performance_dashboard.js
@@ -1,6 +1,8 @@
 frappe.pages["vetedge-branch-performance-dashboard"].on_page_load = function (wrapper) {
-	const page = frappe.ui.make_app_page({ parent: wrapper, title: __("Branch Performance Dashboard"), single_column: true });
-	frappe.require("/assets/vetedge/js/dashboard_shell.js", function () {
-		window.vetedgeDashboardShell.mount(page, { key: "branch_performance", title: __("Branch Performance Dashboard") });
+	frappe.require("vetedge_dashboard_host.bundle.js", function () {
+		window.mountVetEdgeDashboardHost(wrapper, {
+			key: "branch_performance",
+			title: __("Branch Performance Dashboard"),
+		});
 	});
 };
