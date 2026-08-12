1499d34932362e2d7cc16dc29d7cb9ce044bfa04
olayemigod <olayemigod@gmail.com>
2026-08-13 00:26:19 +0100

    feat(vetedge): load dashboard alignment on clinical dashboard

--- vetedge/veterinary/page/vetedge_clinical_dashboard/vetedge_clinical_dashboard.js
+++ vetedge/veterinary/page/vetedge_clinical_dashboard/vetedge_clinical_dashboard.js
@@ -1,8 +1,11 @@
 frappe.pages["vetedge-clinical-dashboard"].on_page_load = function (wrapper) {
 	frappe.require("vetedge_dashboard_host.bundle.js", function () {
-		window.mountVetEdgeDashboardHost(wrapper, {
-			key: "clinical",
-			title: __("Clinical Dashboard"),
+		frappe.require("vetedge_dashboard_alignment.bundle.js", function () {
+			window.VetEdgeDashboardAlignment?.install?.();
+			window.mountVetEdgeDashboardHost(wrapper, {
+				key: "clinical",
+				title: __("Clinical Dashboard"),
+			});
 		});
 	});
 };
