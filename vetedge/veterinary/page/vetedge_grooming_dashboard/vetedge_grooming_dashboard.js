10e71024a3eddf30112ae7ecd5b634f6eeaf47ae
olayemigod <olayemigod@gmail.com>
2026-08-13 00:27:22 +0100

    feat(vetedge): load dashboard alignment on grooming dashboard

--- vetedge/veterinary/page/vetedge_grooming_dashboard/vetedge_grooming_dashboard.js
+++ vetedge/veterinary/page/vetedge_grooming_dashboard/vetedge_grooming_dashboard.js
@@ -1,8 +1,11 @@
 frappe.pages["vetedge-grooming-dashboard"].on_page_load = function (wrapper) {
 	frappe.require("vetedge_dashboard_host.bundle.js", function () {
-		window.mountVetEdgeDashboardHost(wrapper, {
-			key: "grooming",
-			title: __("Grooming Dashboard"),
+		frappe.require("vetedge_dashboard_alignment.bundle.js", function () {
+			window.VetEdgeDashboardAlignment?.install?.();
+			window.mountVetEdgeDashboardHost(wrapper, {
+				key: "grooming",
+				title: __("Grooming Dashboard"),
+			});
 		});
 	});
 };
