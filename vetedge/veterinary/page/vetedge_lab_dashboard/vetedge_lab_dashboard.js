ebdba69538d10eb7bc87b3dcdb1f323012abb051
olayemigod <olayemigod@gmail.com>
2026-08-12 20:43:21 +0100

    feat(vetedge): mount lab dashboard in EdgeSuite shell

--- vetedge/veterinary/page/vetedge_lab_dashboard/vetedge_lab_dashboard.js
+++ vetedge/veterinary/page/vetedge_lab_dashboard/vetedge_lab_dashboard.js
@@ -1,6 +1,8 @@
 frappe.pages["vetedge-lab-dashboard"].on_page_load = function (wrapper) {
-	const page = frappe.ui.make_app_page({ parent: wrapper, title: __("Lab Dashboard"), single_column: true });
-	frappe.require("/assets/vetedge/js/dashboard_shell.js", function () {
-		window.vetedgeDashboardShell.mount(page, { key: "lab", title: __("Lab Dashboard") });
+	frappe.require("vetedge_dashboard_host.bundle.js", function () {
+		window.mountVetEdgeDashboardHost(wrapper, {
+			key: "lab",
+			title: __("Lab Dashboard"),
+		});
 	});
 };
