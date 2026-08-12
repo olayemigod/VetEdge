1c92686ae52c076f27d888ad13d269fa729b1917
olayemigod <olayemigod@gmail.com>
2026-08-12 20:43:42 +0100

    feat(vetedge): mount boarding dashboard in EdgeSuite shell

--- vetedge/veterinary/page/vetedge_boarding_dashboard/vetedge_boarding_dashboard.js
+++ vetedge/veterinary/page/vetedge_boarding_dashboard/vetedge_boarding_dashboard.js
@@ -1,6 +1,8 @@
 frappe.pages["vetedge-boarding-dashboard"].on_page_load = function (wrapper) {
-	const page = frappe.ui.make_app_page({ parent: wrapper, title: __("Boarding Dashboard"), single_column: true });
-	frappe.require("/assets/vetedge/js/dashboard_shell.js", function () {
-		window.vetedgeDashboardShell.mount(page, { key: "boarding", title: __("Boarding Dashboard") });
+	frappe.require("vetedge_dashboard_host.bundle.js", function () {
+		window.mountVetEdgeDashboardHost(wrapper, {
+			key: "boarding",
+			title: __("Boarding Dashboard"),
+		});
 	});
 };
