608fdfa085839805d28f87a2c217a7a2874ee581
olayemigod <olayemigod@gmail.com>
2026-08-13 00:26:37 +0100

    feat(vetedge): load dashboard alignment on inventory dashboard

--- vetedge/veterinary/page/vetedge_inventory_dispensary_dashboard/vetedge_inventory_dispensary_dashboard.js
+++ vetedge/veterinary/page/vetedge_inventory_dispensary_dashboard/vetedge_inventory_dispensary_dashboard.js
@@ -1,8 +1,11 @@
 frappe.pages["vetedge-inventory-dispensary-dashboard"].on_page_load = function (wrapper) {
 	frappe.require("vetedge_dashboard_host.bundle.js", function () {
-		window.mountVetEdgeDashboardHost(wrapper, {
-			key: "inventory_dispensary",
-			title: __("Inventory / Dispensary Dashboard"),
+		frappe.require("vetedge_dashboard_alignment.bundle.js", function () {
+			window.VetEdgeDashboardAlignment?.install?.();
+			window.mountVetEdgeDashboardHost(wrapper, {
+				key: "inventory_dispensary",
+				title: __("Inventory / Dispensary Dashboard"),
+			});
 		});
 	});
 };
