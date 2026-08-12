91d6bd8ec6fd8713fe0287fa414e5b245da885a7
olayemigod <olayemigod@gmail.com>
2026-08-13 00:26:47 +0100

    feat(vetedge): load dashboard alignment on lab dashboard

--- vetedge/veterinary/page/vetedge_lab_dashboard/vetedge_lab_dashboard.js
+++ vetedge/veterinary/page/vetedge_lab_dashboard/vetedge_lab_dashboard.js
@@ -1,8 +1,11 @@
 frappe.pages["vetedge-lab-dashboard"].on_page_load = function (wrapper) {
 	frappe.require("vetedge_dashboard_host.bundle.js", function () {
-		window.mountVetEdgeDashboardHost(wrapper, {
-			key: "lab",
-			title: __("Lab Dashboard"),
+		frappe.require("vetedge_dashboard_alignment.bundle.js", function () {
+			window.VetEdgeDashboardAlignment?.install?.();
+			window.mountVetEdgeDashboardHost(wrapper, {
+				key: "lab",
+				title: __("Lab Dashboard"),
+			});
 		});
 	});
 };
