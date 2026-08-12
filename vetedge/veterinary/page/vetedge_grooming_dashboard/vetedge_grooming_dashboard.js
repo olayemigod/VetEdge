6c1e01afdbd3456791d24eb7b288eeb408584fc1
olayemigod <olayemigod@gmail.com>
2026-08-12 20:43:54 +0100

    feat(vetedge): mount grooming dashboard in EdgeSuite shell

--- vetedge/veterinary/page/vetedge_grooming_dashboard/vetedge_grooming_dashboard.js
+++ vetedge/veterinary/page/vetedge_grooming_dashboard/vetedge_grooming_dashboard.js
@@ -1,6 +1,8 @@
 frappe.pages["vetedge-grooming-dashboard"].on_page_load = function (wrapper) {
-	const page = frappe.ui.make_app_page({ parent: wrapper, title: __("Grooming Dashboard"), single_column: true });
-	frappe.require("/assets/vetedge/js/dashboard_shell.js", function () {
-		window.vetedgeDashboardShell.mount(page, { key: "grooming", title: __("Grooming Dashboard") });
+	frappe.require("vetedge_dashboard_host.bundle.js", function () {
+		window.mountVetEdgeDashboardHost(wrapper, {
+			key: "grooming",
+			title: __("Grooming Dashboard"),
+		});
 	});
 };
