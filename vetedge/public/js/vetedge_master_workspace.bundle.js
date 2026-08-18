de9c3e867b602c0e6de20212c43f6581274ce189
olayemigod <olayemigod@gmail.com>
2026-08-18 21:13:51 +0100

    fix(vetedge): keep master workspace on canonical desk route

--- vetedge/public/js/vetedge_master_workspace.bundle.js
+++ vetedge/public/js/vetedge_master_workspace.bundle.js
@@ -1,6 +1,22 @@
 import VetEdgeMasterWorkspace from './vetedge_master_workspace/VetEdgeMasterWorkspace.vue';
 import { applyWorkspaceSafety } from './vetedge_workspace_safety';
 
+function installCanonicalDeskLocation(component, path) {
+	const methods = component?.methods;
+	if (!methods || component.__vetedgeCanonicalDeskLocationInstalled) return component;
+	const writeLocation = (method, params = {}) => {
+		const url = new URL(window.location.href);
+		url.pathname = path;
+		url.search = new URLSearchParams(params).toString();
+		window.history?.[method]?.(window.history.state, '', `${url.pathname}${url.search}${url.hash}`);
+	};
+	methods.pushLocation = function (params) { writeLocation('pushState', params); };
+	methods.replaceLocation = function (params) { writeLocation('replaceState', params); };
+	component.__vetedgeCanonicalDeskLocationInstalled = true;
+	return component;
+}
+
+installCanonicalDeskLocation(VetEdgeMasterWorkspace, '/desk/vetedge-master-workspace');
 applyWorkspaceSafety(VetEdgeMasterWorkspace);
 
 export function mountVetEdgeMasterWorkspace(target) {
