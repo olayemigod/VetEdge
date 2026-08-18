29f4f9a89b7e66f1ef910646f16455db4715033b
olayemigod <olayemigod@gmail.com>
2026-08-18 14:41:23 +0100

    fix(vetedge): prevent duplicate consultation lab tests

--- vetedge/veterinary/doctype/veterinary_lab_order/veterinary_lab_order.py
+++ vetedge/veterinary/doctype/veterinary_lab_order/veterinary_lab_order.py
@@ -8,6 +8,9 @@ from vetedge.services.lab import handle_lab_order_after_insert, handle_lab_order
 class VeterinaryLabOrder(Document):
 	def validate(self) -> None:
 		validate_lab_order(self)
+		from vetedge.services.consultation_related_records import validate_consultation_lab_test_duplicates
+
+		validate_consultation_lab_test_duplicates(self)
 
 	def after_insert(self) -> None:
 		handle_lab_order_after_insert(self)
