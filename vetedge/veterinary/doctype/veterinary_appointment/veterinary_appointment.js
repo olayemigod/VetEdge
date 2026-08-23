f817bacb63f44f45b267b8d34e8a8ed5a9ea297c
olayemigod <olayemigod@gmail.com>
2026-08-23 22:38:16 +0100

    feat(vetedge): make appointment actions service-aware

--- vetedge/veterinary/doctype/veterinary_appointment/veterinary_appointment.js
+++ vetedge/veterinary/doctype/veterinary_appointment/veterinary_appointment.js
@@ -9,19 +9,29 @@ frappe.ui.form.on("Veterinary Appointment", {
 		frm.set_query("practitioner", () => ({
 			query: "vetedge.services.permissions.get_veterinary_doctor_users",
 		}));
+
+		frm.set_query("grooming_service", () => ({
+			filters: { is_active: 1 },
+		}));
+
+		frm.set_query("groomer", () => ({
+			query: "vetedge.services.permissions.get_grooming_staff_users",
+		}));
 	},
 
 	refresh(frm) {
+		const grooming = is_grooming_appointment(frm);
 		set_consultation_link_display(frm);
 
 		if (frm.is_new()) {
 			return;
 		}
 
-		add_consultation_link_actions(frm);
-
-		if (has_service_consultation(frm)) {
-			return;
+		if (!grooming) {
+			add_consultation_link_actions(frm);
+			if (has_service_consultation(frm)) {
+				return;
+			}
 		}
 
 		if (frm.doc.status === "Awaiting Registration") {
@@ -60,7 +70,14 @@ frappe.ui.form.on("Veterinary Appointment", {
 			}, __("Appointment"));
 		}
 
-		if (["Confirmed", "Checked In"].includes(frm.doc.status)) {
+		if (grooming && ["Confirmed", "Checked In", "In Service"].includes(frm.doc.status)) {
+			frm.add_custom_button(__("Create / Open Grooming Session"), () => {
+				start_grooming_session(frm);
+			}, __("Appointment"));
+			return;
+		}
+
+		if (!grooming && ["Confirmed", "Checked In"].includes(frm.doc.status)) {
 			frm.add_custom_button(__("Start Consultation"), () => {
 				start_consultation(frm);
 			}, __("Appointment"));
@@ -85,6 +102,17 @@ frappe.ui.form.on("Veterinary Appointment", {
 			});
 	},
 
+	appointment_type(frm) {
+		if (is_grooming_appointment(frm)) {
+			frm.set_value("practitioner", "");
+			frm.set_value("practitioner_name", "");
+			return;
+		}
+		frm.set_value("grooming_service", "");
+		frm.set_value("groomer", "");
+		frm.set_value("groomer_name", "");
+	},
+
 	practitioner(frm) {
 		if (!frm.doc.practitioner) {
 			frm.set_value("practitioner_name", "");
@@ -96,11 +124,28 @@ frappe.ui.form.on("Veterinary Appointment", {
 			frm.set_value("practitioner_name", full_name || frm.doc.practitioner);
 		});
 	},
+
+	groomer(frm) {
+		if (!frm.doc.groomer) {
+			frm.set_value("groomer_name", "");
+			return;
+		}
+
+		frappe.db.get_value("User", frm.doc.groomer, "full_name").then((result) => {
+			const full_name = result?.message?.full_name;
+			frm.set_value("groomer_name", full_name || frm.doc.groomer);
+		});
+	},
 });
 
+function is_grooming_appointment(frm) {
+	return frm.doc.appointment_type === "Grooming";
+}
+
 function set_consultation_link_display(frm) {
-	frm.toggle_display("follow_up_reference", Boolean(frm.doc.is_follow_up || frm.doc.follow_up_reference));
-	frm.toggle_display("linked_consultation", has_service_consultation(frm));
+	const grooming = is_grooming_appointment(frm);
+	frm.toggle_display("follow_up_reference", !grooming && Boolean(frm.doc.is_follow_up || frm.doc.follow_up_reference));
+	frm.toggle_display("linked_consultation", !grooming && has_service_consultation(frm));
 }
 
 function add_consultation_link_actions(frm) {
@@ -120,7 +165,7 @@ function add_consultation_link_actions(frm) {
 function has_service_consultation(frm) {
 	return Boolean(
 		frm.doc.linked_consultation &&
-			frm.doc.linked_consultation !== frm.doc.follow_up_reference
+		frm.doc.linked_consultation !== frm.doc.follow_up_reference
 	);
 }
 
@@ -163,6 +208,31 @@ function start_consultation(frm) {
 			});
 			frm.reload_doc().then(() => {
 				frappe.set_route("Form", "Veterinary Consultation", consultation.name);
+			});
+		},
+	});
+}
+
+function start_grooming_session(frm) {
+	frappe.call({
+		method: "vetedge.services.appointment_grooming_bridge.create_grooming_session_from_veterinary_appointment",
+		args: {
+			appointment: frm.doc.name,
+		},
+		freeze: true,
+		freeze_message: __("Preparing grooming session..."),
+		callback(result) {
+			const session = result.message;
+			if (!session?.name) {
+				return;
+			}
+
+			frappe.show_alert({
+				message: __(session.created ? "Grooming session created" : "Grooming session opened"),
+				indicator: "green",
+			});
+			frm.reload_doc().then(() => {
+				frappe.set_route("Form", "Pet Grooming Session", session.name);
 			});
 		},
 	});
