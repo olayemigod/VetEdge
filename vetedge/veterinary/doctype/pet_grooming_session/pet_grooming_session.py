0a59e6ec9044b2a0bfd399f3e621174fe0ae459f
olayemigod <olayemigod@gmail.com>
2026-08-23 21:57:20 +0100

    feat(vetedge): support unified grooming appointment sessions

--- vetedge/veterinary/doctype/pet_grooming_session/pet_grooming_session.py
+++ vetedge/veterinary/doctype/pet_grooming_session/pet_grooming_session.py
@@ -7,7 +7,16 @@ from vetedge.services.grooming import handle_grooming_session_on_update, validat
 
 class PetGroomingSession(Document):
 	def validate(self) -> None:
+		if self.get("veterinary_appointment"):
+			from vetedge.services.appointment_grooming_bridge import validate_veterinary_appointment_grooming_session
+
+			validate_veterinary_appointment_grooming_session(self)
+			return
 		validate_grooming_session(self)
 
 	def on_update(self) -> None:
 		handle_grooming_session_on_update(self)
+		if self.get("veterinary_appointment"):
+			from vetedge.services.appointment_grooming_bridge import sync_veterinary_appointment_from_grooming_session
+
+			sync_veterinary_appointment_from_grooming_session(self)
