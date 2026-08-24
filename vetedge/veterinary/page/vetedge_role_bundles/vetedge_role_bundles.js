dceccaa74936aef0404dfdcb5ff464a770131c90
olayemigod <olayemigod@gmail.com>
2026-08-25 00:35:06 +0100

    feat(vetedge): route role bundles to administration

---
+++ vetedge/veterinary/page/vetedge_role_bundles/vetedge_role_bundles.js
@@ -0,0 +1,12 @@
+frappe.pages["vetedge-role-bundles"].on_page_load = function (wrapper) {
+	wrapper.page = frappe.ui.make_app_page({ parent: wrapper, title: __("Role Bundles"), single_column: true });
+};
+frappe.pages["vetedge-role-bundles"].on_page_show = function () {
+	const target = "/desk/vetedge-administration?resource=role-bundles";
+	if (window.history && typeof frappe?.router?.route === "function") {
+		window.history.replaceState(window.history.state, "", target);
+		Promise.resolve(frappe.router.route()).catch(() => window.location.replace(target));
+		return;
+	}
+	window.location.replace(target);
+};
