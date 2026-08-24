73ba7cdfb26fff8f60fcaa3a47c05815a797bd30
olayemigod <olayemigod@gmail.com>
2026-08-25 00:45:53 +0100

    feat(vetedge): route practitioner coverage to shared workspace

---
+++ vetedge/veterinary/page/vetedge_practitioner_coverage/vetedge_practitioner_coverage.js
@@ -0,0 +1,12 @@
+frappe.pages["vetedge-practitioner-coverage"].on_page_load = function (wrapper) {
+	wrapper.page = frappe.ui.make_app_page({ parent: wrapper, title: __("Practitioner Coverage"), single_column: true });
+};
+frappe.pages["vetedge-practitioner-coverage"].on_page_show = function () {
+	const target = "/desk/vetedge-branch-access?resource=practitioner-assignments";
+	if (window.history && typeof frappe?.router?.route === "function") {
+		window.history.replaceState(window.history.state, "", target);
+		Promise.resolve(frappe.router.route()).catch(() => window.location.replace(target));
+		return;
+	}
+	window.location.replace(target);
+};
