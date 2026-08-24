439547cec61c75a5f506edb3cd873f91b157b021
olayemigod <olayemigod@gmail.com>
2026-08-24 20:51:48 +0100

    fix(boarding): enforce checkout billing invariant

--- vetedge/veterinary/doctype/pet_boarding_booking/pet_boarding_booking.py
+++ vetedge/veterinary/doctype/pet_boarding_booking/pet_boarding_booking.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from frappe.model.document import Document
 
 from vetedge.services.boarding import validate_pet_boarding_booking
+from vetedge.services.boarding_billing_release_safety import validate_boarding_checkout_release_safety
 from vetedge.services.boarding_cancellation_safety import enforce_boarding_cancellation_safety
 
 
@@ -10,3 +11,5 @@ class PetBoardingBooking(Document):
 	def validate(self) -> None:
 		enforce_boarding_cancellation_safety(self)
 		validate_pet_boarding_booking(self)
+		if self.status == "Checked Out":
+			validate_boarding_checkout_release_safety(self)
