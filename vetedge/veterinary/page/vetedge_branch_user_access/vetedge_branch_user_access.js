1354446929255be65919a58be027c2228c56ce08
olayemigod <olayemigod@gmail.com>
2026-08-25 21:31:11 +0100

    fix(vetedge): defer administration alias routing

--- vetedge/veterinary/page/vetedge_branch_user_access/vetedge_branch_user_access.js
+++ vetedge/veterinary/page/vetedge_branch_user_access/vetedge_branch_user_access.js
@@ -3,10 +3,12 @@ frappe.pages["vetedge-branch-user-access"].on_page_load = function (wrapper) {
 };
 frappe.pages["vetedge-branch-user-access"].on_page_show = function () {
 	const target = "/desk/vetedge-branch-access?resource=user-assignments";
-	if (window.history && typeof frappe?.router?.route === "function") {
-		window.history.replaceState(window.history.state, "", target);
-		Promise.resolve(frappe.router.route()).catch(() => window.location.replace(target));
-		return;
-	}
-	window.location.replace(target);
+	window.setTimeout(() => {
+		if (window.history && typeof frappe?.router?.route === "function") {
+			window.history.replaceState(window.history.state, "", target);
+			Promise.resolve(frappe.router.route()).catch(() => window.location.replace(target));
+			return;
+		}
+		window.location.replace(target);
+	}, 0);
 };
