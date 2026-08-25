a465541a1116435a889d7a705bc46294523c44b3
olayemigod <olayemigod@gmail.com>
2026-08-25 21:31:19 +0100

    fix(vetedge): defer administration alias routing

--- vetedge/veterinary/page/vetedge_practitioner_coverage/vetedge_practitioner_coverage.js
+++ vetedge/veterinary/page/vetedge_practitioner_coverage/vetedge_practitioner_coverage.js
@@ -3,10 +3,12 @@ frappe.pages["vetedge-practitioner-coverage"].on_page_load = function (wrapper) 
 };
 frappe.pages["vetedge-practitioner-coverage"].on_page_show = function () {
 	const target = "/desk/vetedge-branch-access?resource=practitioner-assignments";
-	if (window.history && typeof frappe?.router?.route === "function") {
-		window.history.replaceState(window.history.state, "", target);
-		Promise.resolve(frappe.router.route()).catch(() => window.location.replace(target));
-		return;
-	}
-	window.location.replace(target);
+	window.setTimeout(() => {
+		if (window.history && typeof frappe?.router?.route === "function") {
+			window.history.replaceState(window.history.state, "", target);
+			Promise.resolve(frappe.router.route()).catch(() => window.location.replace(target));
+			return;
+		}
+		window.location.replace(target);
+	}, 0);
 };
