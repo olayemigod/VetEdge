3a1fae5b4969f009dbf7f1dee67848c537b2bf0d
olayemigod <olayemigod@gmail.com>
2026-09-01 00:09:05 +0100

    fix(vetedge): load native product menu guard globally

--- vetedge/public/js/report_pdf_patch.js
+++ vetedge/public/js/report_pdf_patch.js
@@ -76,5 +76,7 @@ frappe.render_pdf = function (html, opts = {}) {
 	xhr.send(formData);
 };
 
+// Global Desk compatibility guard: product navigation must never survive on native ERPNext pages.
+frappe.require("/assets/vetedge/js/vetedge_product_menu_native_guard.js?v=20260831-1");
 frappe.require("/assets/vetedge/js/vetedge_report_scheduling_ui.js");
 frappe.require("/assets/vetedge/js/vetedge_report_scheduling_management_ui.js");
